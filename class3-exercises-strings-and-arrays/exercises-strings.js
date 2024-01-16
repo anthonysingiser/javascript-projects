28fce49855de87ce39b1d064b4ce1fece0037fcb
chrysanthemum <asingiser@gmail.com>
2024-01-16 13:12:10 -0500

    finish string exercises

--- class3-exercises-strings-and-arrays/exercises-strings.js
+++ class3-exercises-strings-and-arrays/exercises-strings.js
@@ -3,7 +3,7 @@
 // it will not give us the length of a number. If num = 1001, num.length will return undefined
 // rather than 4.
 
-
+//  PART ONE
 const number = 1001;
 const decimal = 123.45;
 
@@ -28,6 +28,7 @@ console.log(numberLength(decimal)); // 5
 
 // strings are immutable, use the trim method to remove whitespace from the beginning and end of a string
 
+// PART TWO
 let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT "
 let noWhiteSpace = dna.trim();
 let upperCase = noWhiteSpace.toUpperCase();
@@ -50,6 +51,19 @@ console.log(`The DNA strand is ${dna.length} characters long.`);
 
 console.log(dna.slice(4,7).toLowerCase() + 'o ' + dna.slice(dna.indexOf('CAT'), dna.indexOf('CAT') + 3).toLowerCase());
 
+// PART THREE
+
+const language = 'JavaScript';
+const initials = 'JS';
+const titleCase = 'title case';
 
 
+console.log(language.slice(0,1) + language.slice(4,5));
+console.log(language.replace('ava','').replace('cript',''));
+console.log(language[language.indexOf('J')] + language[language.indexOf('S')]);
 
+console.log(`The abbreviation for '${language}' is '${initials}'.`);
+
+console.log(language.slice(1,language.length).replace('a', '@').toUpperCase());
+
+console.log(titleCase.replace('t', 'T').replace('c', 'C'));
